b7885f03b3dc1cb3749ee4e48e4d984f12450e51
belocer <belocerkovecden@gmail.com>
2020-10-09 11:29:14 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import '../styles/style.css';
+import '../styles/style.less';
+import '../styles/style.scss';
+import '../styles/style.sass';
+
+console.log('Works!');
+
+import Person from './person.js'
+const Elena = new Person('Elena', 62, 'Russia')
+console.log(Elena)
